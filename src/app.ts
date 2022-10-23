a7cfa5f0afad51b5c9d879ef2209a6335d641d41
tim.gragg <tim.gragg@asurion.com>
2022-10-23 09:11:52 -0500

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import { feathers } from '@feathersjs/feathers'
+import { koa, rest, bodyParser, errorHandler } from '@feathersjs/koa'
+import serveStatic from 'koa-static'
+import PeopleService from "./services/people-service";
+import StarshipService from "./services/starship-service";
+
+// This tells TypeScript what services we are registering
+type ServiceTypes = {
+    people: PeopleService
+    starships: StarshipService
+}
+
+// Creates an ExpressJS compatible Feathers application
+const app = koa<ServiceTypes>(feathers())
+
+// Use the current folder for static file hosting
+app.use(serveStatic('.'))
+// Register the error handle
+app.use(errorHandler())
+// Parse JSON request bodies
+app.use(bodyParser())
+
+// Register REST service handler
+app.configure(rest())
+// Register our services
+const peopleService = new PeopleService();
+app.use('people', peopleService)
+app.use('starships', new StarshipService(peopleService))
+
+// Start the server
+app.listen(3030).then(() => console.log('Feathers server listening on localhost:3030'))
